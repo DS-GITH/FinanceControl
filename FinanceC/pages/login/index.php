aa4624d065ffc9cf009ad3bab4bdc67011ae555c
Sophia <david.tecds@gmail.com>
2025-05-26 09:48:58 -0300

    created

---
+++ FinanceC/pages/login/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<main> 
+
+    <h1>Login</h1>
+
+
+    <form action="login.php" method="post">
+        
+        <label for="username">Usuário:</label>
+        <input type="text" id="username" name="username" required>
+        
+        <label for="password">Senha:</label>
+        <input type="password" id="password" name="password" required>
+        
+        <button type="submit">Entrar</button>
+
+    </form>
+
+</main>
+    
+</body>
+</html>
